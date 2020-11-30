c2c96bac4fd9f0ea7fca517dc9711d9775d7ea38
amaraladil <amaraladil@hotmail.com>
2020-11-29 20:18:28 -0500

    More updates

--- routes/index.js
+++ routes/index.js
@@ -33,7 +33,7 @@ router.get('/project', function(req, res, next) {
 		name: "King William Hotel",
 		description: "Small website project with Ruby on Rails and is designed to book appointments for customers.",
 		date: "December 2017",
-		source: "RubyOnRails",
+		tech: "RubyOnRails",
 		link: "#",
 		image: ""
 	},
@@ -41,7 +41,7 @@ router.get('/project', function(req, res, next) {
 		name: "Theater Project",
 		description: "Visual Basic Project that displays a theater room, where users can book seats, and show reports for each theater schedule.",
 		date: "April 2016",
-		source: "VB, MS SQL, Visual Studio",
+		tech: "VB, MS SQL, Visual Studio",
 		link: "#",
 		image: ""
 	},
@@ -49,7 +49,7 @@ router.get('/project', function(req, res, next) {
 		name: "Realtor Website",
 		description: "A web development class group project that is designed for realtors. Was the project planner/lead, focused on the backend ",
 		date: "December 2016",
-		source: "PHP, Postgre SQL",
+		tech: "PHP, Postgre SQL",
 		link: "https://realtorwebsite-project.herokuapp.com/",
 		image: "realtor.PNG"
 	},
@@ -57,7 +57,7 @@ router.get('/project', function(req, res, next) {
 		name: "Durak Game",
 		description: "Used C# to design and program the game to functionally working.",
 		date: "April 2017",
-		source: "C#, Visual Studio",
+		tech: "C#, Visual Studio",
 		link: "#",
 		image: ""
 	},
@@ -65,7 +65,7 @@ router.get('/project', function(req, res, next) {
 		name: "Cobol Transactions Paper",
 		description: "A mainframe program that seperates each transaction records to specific webpages.",
 		date: "April 2017",
-		source: "COBOL",
+		tech: "COBOL",
 		link: "#",
 		image: ""
 	},
@@ -73,7 +73,7 @@ router.get('/project', function(req, res, next) {
 		name: "Portfolio",
 		description: "The current website - used Node, Express and Pug to create this website with mongoDB and mail service",
 		date: "July 2019",
-		source: "Node, MongoDB, Pug",
+		tech: "Node, MongoDB, Pug",
 		link: "/",
 		image: "PortfolioPage.PNG"
 	}
